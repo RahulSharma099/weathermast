9fc432177beac3bb538249b487eb6db6b29f2e0c
Rahul Sharma <90137rahul@gmail.com>
2020-10-29 04:40:11 +0530

    Routes setup for help and about

--- src/app.js
+++ src/app.js
@@ -1,27 +1,44 @@
-const path = require('path')
-const express = require('express')
+const path = require("path");
+const express = require("express");
 
-const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
+const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.set('view engine', 'hbs')
-app.use(express.static(publicDirectoryPath))
+app.set("view engine", "hbs");
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index')
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather app",
+    name: "Rahul Sharma",
+  });
+});
 
-app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is raining',
-        location: 'Delhi'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Rahul Sharma",
+  });
+});
 
-// app.com 
+app.get("/help", (req, res) => {
+  res.render("help", {
+    help1: "Help for weather app!",
+    help2: "help for about me!!",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is raining",
+    location: "Delhi",
+  });
+});
+
+// app.com
 // app.com/help
 // app.com/about
 
 app.listen(5000, () => {
-    console.log('Server started at port 5000');
-})
+  console.log("Server started at port 5000");
+});
