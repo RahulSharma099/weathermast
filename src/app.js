8bd266a4f73d25c14c323e643da383692317a292
RahulSharma099 <90137rahul@gmail.com>
2021-06-30 00:10:39 +0530

    🚑 Added routes for diffrent pages

--- src/app.js
+++ src/app.js
@@ -1,85 +1,76 @@
-const path = require("path");
-const express = require("express");
-const hbs = require("hbs");
-const geocode = require("./utils/geocode");
-const forecast = require("./utils/forecast");
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
 // Define paths for express config
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // setup handlebars engine and views location
-app.set("view engine", "hbs");
-app.set("views", viewsPath);
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get("", (req, res) => {
-  res.render("index", {
-    title: "Weather app",
-    name: "Rahul Sharma",
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather app',
+    name: 'Rahul Sharma',
   });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", {
-    title: "About me",
-    name: "Rahul Sharma",
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Rahul Sharma',
   });
 });
 
-app.get("/help", (req, res) => {
-  res.render("help", {
-    helpText: "This is some helpful text",
-    title: "Help",
-    name: "Rahul Sharma",
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'This is some helpful text',
+    title: 'Help',
+    name: 'Rahul Sharma',
   });
 });
 
-app.get("/weather", (req, res) => {
+app.get('/weather', (req, res) => {
   if (!req.query.address) {
     return res.send({
-      error: "You must provide a location (:",
+      error: 'You must provide a location (:',
     });
   }
 
-  geocode(
-    req.query.address,
-    (error, { latitude, longitude, location } = {}) => {
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return res.send({ error });
       }
 
-      forecast(latitude, longitude, (error, forecastData) => {
-        if (error) {
-          return res.send({ error });
-        }
-
-        res.send({
-          forecast: forecastData,
-          location,
-          address: req.query.address,
-        });
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
       });
-    }
-  );
-
-  //   res.send({
-  //     forecast: "It is raining",
-  //     location: "Delhi",
-  //     address: req.query.address,
-  //   });
+    });
+  });
 });
 
-app.get("/products", (req, res) => {
+app.get('/products', (req, res) => {
   if (!req.query.search) {
     return res.send({
-      error: "You must provide a search term",
+      error: 'You must provide a search term',
     });
   }
 
@@ -88,22 +79,22 @@ app.get("/products", (req, res) => {
   });
 });
 
-app.get("/help/*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    helpMessage: "Help Article not found (:",
-    name: "Rahul Sharma",
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    helpMessage: 'Help Article not found (:',
+    name: 'Rahul Sharma',
   });
 });
 
-app.get("*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    helpMessage: "Page not found",
-    name: "Rahul Sharma",
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    helpMessage: 'Page not found',
+    name: 'Rahul Sharma',
   });
 });
 
 app.listen(5000, () => {
-  console.log("Server started at port 5000");
+  console.log('Server started at port 5000');
 });
