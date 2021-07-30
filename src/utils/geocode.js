f53f5e207ddd2cb81da9f06d54649b2d21392d01
Rahul Sharma <90137rahul@gmail.com>
2021-07-30 16:15:29 +0530

    🐛 Fix a bug

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,5 +1,6 @@
-const request = require("request");
+const axios = require("axios");
 
+/** 
 const geocode = (address, callback) => {
   const url =
     "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
@@ -19,6 +20,30 @@ const geocode = (address, callback) => {
       });
     }
   });
+};
+*/
+
+const geocode = async (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURIComponent(address) +
+    ".json?&access_token=pk.eyJ1Ijoic2xhcG1hY2hpbmUiLCJhIjoiY2tld2YzMnk5M2gyNTJybXFidTJjODZmaSJ9.XzszQZ7S_wynFOeX7mZgwQ&limit=1";
+
+  const { data } = await axios({
+    method: "get",
+    url: url,
+  });
+  if (!data) {
+    return callback("Unable to find Location. Try another Search!");
+  } else if (data.features.length === 0) {
+    callback("Unable to find location. Try another Search!");
+  } else {
+    callback(undefined, {
+      latitude: data.features[0].center[1],
+      longitude: data.features[0].center[0],
+      location: data.features[0].place_name,
+    });
+  }
 };
 
 module.exports = geocode;
